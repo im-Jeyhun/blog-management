fde6977c364465d4a1c3ea935b49ca72af81111b
Ceyhun <contact.ceyhun@gmail.com>
2022-07-28 21:29:46 +0400

    Blog model added

---
+++ blogManagement/blogManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace blogManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
